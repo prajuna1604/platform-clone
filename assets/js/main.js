4c2d29c28a451b58144a50257094e8c4b9420bb3
prajuna <prajuna.chalise@gmail.com>
2025-11-12 10:47:12 +0545

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+// document.addEventListener('DOMContentLoaded', () => {
+// // Wait for DOM
+// const body=document.querySelector('body');
+// const nav=document.querySelector('nav');
+// const modeToggle=document.querySelector('.dark-light');
+// const searchToggle=document.querySelector('.searchBox');
+// modeToggle.addEventListener('click',()=>{
+//   modeToggle.classList.toggle('active');
+// });
+// });
+ document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+ 
+  const modeToggle = document.querySelector('.dark-light');
+
+  if (modeToggle) {
+    modeToggle.addEventListener('click', () => {
+      modeToggle.classList.toggle('active');
+      body.classList.toggle('dark-mode');
+      
+    });
+  }
+  const burgerToggle = document.getElementById("burgerToggle");
+  const burgerOverlay = document.getElementById("burgerOverlay");
+
+  burgerToggle.addEventListener("click", () => {
+    burgerOverlay.classList.toggle("active");
+  });
+
+  // Optional: close when clicking outside or pressing ESC
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") burgerOverlay.classList.remove("active");
+  });
+  const demosMenu = document.querySelector(".demos-menu");
+  const featuresMenu = document.querySelector(".features-menu");
+  
+
+const searchBox = document.querySelector(".searchBox");
+  const searchContainer = document.querySelector(".cs-col-search");
+  const closeBtn = document.querySelector(".cs-search-close");
+
+  searchBox.addEventListener("click", () => {
+    searchContainer.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    searchContainer.classList.remove("active");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!searchContainer.contains(e.target) && !searchBox.contains(e.target)) {
+      searchContainer.classList.remove("active");
+    }
+  });
+
+
+});
+
